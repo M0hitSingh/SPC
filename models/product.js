beff2175966e933cd9093a9fb8244ddc6aacd460
bhavya3113 <agrawalbhavya3113@gmail.com>
2022-11-17 19:48:10 +0530

    added product model

---
+++ models/product.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const productSchema = new schema({
+  name:{
+    type: String,
+    require: true
+  },
+  // category:{
+  //   type: String,
+  //   require: true
+  // },
+  quantity:{
+    type: Number,
+    require: true
+  },
+  description:{
+    type: String,
+    require: true
+  },
+  price:{
+    type: Number,
+    require:true
+  },
+  imageUrl:{
+    type: String,
+    require:true
+  },
+  eachrating:[{
+    user:{ 
+      type: schema.Types.ObjectId,
+      ref:'users'
+    },
+    rate:{
+    type: Number,
+    require: false,
+    min:1,
+    max:5,
+    },
+    userreview:{
+      type: String,
+      require: false,
+    },
+  }],
+  avgrating:{ 
+      type: Number,
+      require: false,
+  }
+}
+)
+
+module.exports = mongoose.model("product",productSchema);
